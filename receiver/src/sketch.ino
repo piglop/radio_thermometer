b6fe768bd9087863889ff132e8ecafd73d2bb080
Michaël Witrant <mike@lepton.fr>
2013-04-29 21:02:15 +0200

    cleanup

--- receiver/src/sketch.ino
+++ receiver/src/sketch.ino
@@ -1,20 +1,12 @@
-// receiver.pde
-//
-// Simple example of how to use VirtualWire to receive messages
-// Implements a simplex (one-way) receiver with an Rx-B1 module
-//
-// See VirtualWire.h for detailed API docs
-// Author: Mike McCauley (mikem@airspayce.com)
-// Copyright (C) 2008 Mike McCauley
-// $Id: receiver.pde,v 1.3 2009/03/30 00:07:24 mikem Exp $
 #include <VirtualWire.h>
+
 void setup()
 {
-  Serial.begin(115200); // Debugging only
+  Serial.begin(115200);
   Serial.println("setup");
   pinMode(11, INPUT);
   pinMode(13, OUTPUT);
-  // Initialise the IO and ISR
+  
   vw_set_ptt_inverted(true); // Required for DR3100
   vw_setup(2000);    // Bits per sec
   vw_set_rx_pin(11);
@@ -27,6 +19,7 @@ void loop()
 {
   uint8_t buf[VW_MAX_MESSAGE_LEN];
   uint8_t buflen = VW_MAX_MESSAGE_LEN;
+  
   if (vw_get_message(buf, &buflen)) // Non-blocking
   {
     int i;
--- thermometer/src/sketch.ino
+++ thermometer/src/sketch.ino
@@ -15,141 +15,40 @@ void setup(void) {
   vw_setup(2000);      // Bits per sec
 }
 
-char * floatToString(char * outstr, double val, byte precision, byte widthp){
-  char temp[16];
-  byte i;
-
-  // compute the rounding factor and fractional multiplier
-  double roundingFactor = 0.5;
-  unsigned long mult = 1;
-  for (i = 0; i < precision; i++)
-  {
-    roundingFactor /= 10.0;
-    mult *= 10;
-  }
-
-  temp[0]='\0';
-  outstr[0]='\0';
-
-  if(val < 0.0){
-    strcpy(outstr,"-\0");
-    val = -val;
-  }
-
-  val += roundingFactor;
-
-  strcat(outstr, itoa(int(val),temp,10));  //prints the int part
-  if( precision > 0) {
-    strcat(outstr, ".\0"); // print the decimal point
-    unsigned long frac;
-    unsigned long mult = 1;
-    byte padding = precision -1;
-    while(precision--)
-      mult *=10;
-
-    if(val >= 0)
-      frac = (val - int(val)) * mult;
-    else
-      frac = (int(val)- val ) * mult;
-    unsigned long frac1 = frac;
-
-    while(frac1 /= 10)
-      padding--;
-
-    while(padding--)
-      strcat(outstr,"0\0");
-
-    strcat(outstr,itoa(frac,temp,10));
-  }
-
-  // generate space padding 
-  if ((widthp != 0)&&(widthp >= strlen(outstr))){
-    byte J=0;
-    J = widthp - strlen(outstr);
-
-    for (i=0; i< J; i++) {
-      temp[i] = ' ';
-    }
-
-    temp[i++] = '\0';
-    strcat(temp,outstr);
-    strcpy(outstr,temp);
-  }
-
-  return outstr;
-}
-
 void loop(void) {
   byte i;
   byte present = 0;
   byte data[12];
   byte addr[8];
 
-
   if ( !ds.search(addr)) {
-//      Serial.print("No more addresses.\n");
       ds.reset_search();
       delay(1000);
       return;
   }
-  /*
-  Serial.print("R=");
-  for( i = 0; i < 8; i++) {
-    Serial.print(addr[i], HEX);
-    Serial.print(" ");
-  }
-  */
+  
   if ( OneWire::crc8( addr, 7) != addr[7]) {
       Serial.print("CRC is not valid!\n");
       return;
   }
-/*
-  if ( addr[0] == 0x10) {
-      Serial.print("Device is a DS18S20 family device.\n");
-  }
-  else if ( addr[0] == 0x28) {
-      Serial.print("Device is a DS18B20 family device.\n");
-  }
-  else {
-      Serial.print("Device family is not recognized: 0x");
-      Serial.println(addr[0],HEX);
-      return;
-  }
-*/
+  
   ds.reset();
   ds.select(addr);
-  ds.write(0x44,1);         // start conversion, with parasite power on at the end
+  ds.write(0x44,1); // start conversion, with parasite power on at the end
 
-  delay(1000);     // maybe 750ms is enough, maybe not
+  delay(1000); // maybe 750ms is enough, maybe not
   // we might do a ds.depower() here, but the reset will take care of it.
 
   present = ds.reset();
   ds.select(addr);    
-  ds.write(0xBE);         // Read Scratchpad
-/*
-  Serial.print("P=");
-  Serial.print(present,HEX);
-  Serial.print(" ");
-*/
-  for ( i = 0; i < 9; i++) {           // we need 9 bytes
+  ds.write(0xBE); // Read Scratchpad
+  
+  for ( i = 0; i < 9; i++) {
     data[i] = ds.read();
-  //  Serial.print(data[i], HEX);
-  //  Serial.print(" ");
   }
-/*
-  Serial.print(" CRC=");
-  Serial.print( OneWire::crc8( data, 8), HEX);
-  Serial.println();
-*/
+  
   int low = data[0];
   int high = data[1];
-  /*
-  float temp = (float)((high << 8) | low) / 16.0;
-
-  Serial.print("TEMP=");
-  Serial.print(temp);
-  Serial.println();
-  */
 
   unsigned char msg[16];
   int len = 0;
@@ -163,19 +62,9 @@ void loop(void) {
     msg[len++] = data[i];
   }
   
-  /*
-  Serial.print("MSG=");
-  for( i = 0; i < len; i++) {
-    Serial.print(msg[i], HEX);
-    Serial.print(" ");
-  }
-  Serial.println();
-  */
-
-
-  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(13, HIGH);
   vw_send((uint8_t *)msg, len);
-  vw_wait_tx(); // Wait until the whole message is gone
+  vw_wait_tx();
   delay(100);
-  digitalWrite(13, LOW);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(13, LOW);
 }
