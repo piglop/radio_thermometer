2a37d6f0e566794a356ea4cadf91f73320fcdd24
Michael Witrant <michael@ouvrages-web.fr>
2013-09-29 20:17:59 +0200

    working usb reading from digispark

--- digispark_receiver/src/sketch.ino
+++ digispark_receiver/src/sketch.ino
@@ -1,8 +1,20 @@
+#include <DigiUSB.h>
 
 void setup()
 {
+  DigiUSB.begin();
+  pinMode(0, OUTPUT); //LED on Model B
+  pinMode(1, OUTPUT); //LED on Model A   
 }
 
 void loop()
 {
+  digitalWrite(0, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(1, HIGH);
+  DigiUSB.refresh();
+  DigiUSB.println("Hello");
+  delay(250);
+  digitalWrite(0, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(1, LOW); 
+  delay(250);
 }
